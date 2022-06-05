a71c01dc9a098093dce9535e85772d4ffcb2dca4
Julieta Bernaus <76222131+jberlar@users.noreply.github.com>
2022-06-05 17:41:08 -0300

    added tests to cell

--- test/tc_cell.rb
+++ test/tc_cell.rb
@@ -1,19 +1,23 @@
 require 'minitest/autorun'
+
 require_relative '../lib/parchis/parchis'
 require_relative '../lib/parchis/cell'
+require_relative '../lib/parchis/token'
 
 class TestCell < MiniTest::Test
 
+    
     def setup
         @cell= Cell.new(1)
+        @token=Token.new(:red,@cell,'A')
     end
 
     def test_place_token
-        
+        assert @cell.place_token(@token)
     end
 
     def test_empty?
-           
+        refute @cell.empty?
     end
 
     def test_get_length_coords
--- test/tc_lobby_updater.rb
+++ test/tc_lobby_updater.rb
@@ -1,13 +1,15 @@
+require 'minitest/autorun'
 require_relative '../lib/parchis/lobby_updater'
 
 class TestLobbyUpdater < MiniTest::Test
 
+    
     def setup
-
+        
     end
     
     def test_join_lobby
-
+        #idk how to test this
     end
 
 end
