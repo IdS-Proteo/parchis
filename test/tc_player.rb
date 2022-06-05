c09b09ab4d73c6c7a19aaaf3c8eb50c4a987baf7
Julieta Bernaus <76222131+jberlar@users.noreply.github.com>
2022-06-05 12:51:22 -0300

    tc_player running

--- test/tc_player.rb
+++ test/tc_player.rb
@@ -1,13 +1,18 @@
+require 'minitest/autorun'
 require_relative '../lib/parchis/player'
 
 class TestPlayer < MiniTest::Test
 
-    def test_to_s
+    def setup
+        @player = Player.new( name: 'self',local: false, host:false)
+    end
 
+    def test_to_s
+        assert_equal 'self',@player.to_s
     end
 
     def test_can_roll_dice?
-
+        refute @player.can_roll_dice?
     end
 
 end
