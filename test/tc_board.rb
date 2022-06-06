4a43fe2dea160ae6ea6c8577346cf32a0d90bca3
Computadora504 <80212561+Computadora504@users.noreply.github.com>
2022-06-05 22:56:37 -0300

    Update tc_board.rb

--- test/tc_board.rb
+++ test/tc_board.rb
@@ -2,39 +2,55 @@ require 'minitest/autorun'
 require_relative '../lib/parchis/board'
 require_relative '../lib/parchis/player'
 require_relative '../lib/parchis/token'
-require_relative '../lib/parchis/cell'
 require_relative '../lib/parchis/parchis'
+require_relative '../lib/parchis/cell'
 
 
 class TestBoard < MiniTest::Test
 
     def setup
-        @board=Board.new([player1,player2,player3,player4])
+        @player1 = Player.new(name: 'Self', local: true, host: !!@hosting)
+        @player2 = Player.new(name: 'Foolano1', local: false, host: false)
+        @player3 = Player.new(name: 'Foolano2', local: false, host: false)
+        @player4 = Player.new(name: 'Foolano3', local: false, host: false)
+        @players = [@player1, @player2, @player3, @player4]
+        @board = Board.new(@players, player_turn: nil)
     end
 
     def test_next_turn
-        assert_includes([0,1,2,3],player1.next_turn())
+        assert_equal(1, @board.next_turn)
+        assert_equal(2, @board.next_turn)
+        assert_equal(3, @board.next_turn)
+        assert_equal(0, @board.next_turn)
     end
 
 
     def test_player_turn
-        assert_equal(player1,player_turn)
+        assert_equal('Self', @board.player_turn.to_s)
     end
 
-    def test_debug_tokens_positioning
+    #def test_debug_tokens_positioning
         
-    end
+    #end
 
     def test_assign_tokens_to_players
-        
+        @board.send(:assign_tokens_to_players)
+        assert_equal 4, @player1.tokens.length
+        assert_equal 4, @player2.tokens.length
+        assert_equal 4, @player3.tokens.length
+        assert_equal 4, @player4.tokens.length
+
     end
 
     def test_initialize_cells
-
+        assert_equal 117, @board.cells.size
     end
 
     def test_assign_color_to_players
-        
+        assert_kind_of Symbol, @player1.color
+        assert_kind_of Symbol, @player2.color
+        assert_kind_of Symbol, @player3.color
+        assert_kind_of Symbol, @player4.color
     end
 
 end
